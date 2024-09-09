6c53ffceadec9fe0a3b86ad6a1699bfbcfeba10f
zoeznm <bomikim046@gmail.com>
2024-09-09 11:50:52 +0900

    python 연습하기

---
+++ inout.py
@@ -0,0 +1,14 @@
+# * 배열로 접근해서 내 이름의 lastname, firstname 찾아내기 
+
+
+full_name = "김 보미"
+
+
+name_parts = full_name.split()
+
+
+lastname = name_parts[0]  
+firstname = name_parts[1]  
+
+print("Lastname:", lastname)
+print("Firstname:", firstname)
---
+++ testOne.py
@@ -0,0 +1,29 @@
+a = "김보미"
+b= "미남"
+
+def stringMerge () : 
+    global a, b
+    return a + b
+
+print(stringMerge(a,b))
+
+
+# * 맨날 이름 쓰기 힘듦
+def example ():
+    return "예제"
+
+anther_example = lambda: "예제"
+
+
+
+
+array = ["배성빈", "문혜림", "신지윤"]
+
+object = {
+  "조자연" : "안녕",
+  "김보미" : "개굴",
+  "황재민" : "수면왕",
+  "황재민" : "수면왕"
+}
+
+another_obj = ("김정수", "송이현", "최유진")
